0223787e43338e7083ffd4394822e51b59475e90
Michael Zhao <michaelzhao314@gmail.com>
2023-03-01 12:42:21 -0600

    Center stats

--- client/src/components/admin/AdminStat.tsx
+++ client/src/components/admin/AdminStat.tsx
@@ -1,8 +1,12 @@
-const AdminStat = (props: { name: string; value: string | number }) => {
-    return <div className="text-center">
-        <div className="text-6xl">{props.value}</div>
-        <div className="text-light">{props.name}</div>
-    </div>;
+import { twMerge } from "tailwind-merge";
+
+const AdminStat = (props: { name: string; value: string | number; className?: string }) => {
+    return (
+        <div className={twMerge("text-center", props.className)}>
+            <div className="text-6xl">{props.value}</div>
+            <div className="text-light">{props.name}</div>
+        </div>
+    );
 };
 
 export default AdminStat;
--- client/src/components/admin/AdminStatsPanel.tsx
+++ client/src/components/admin/AdminStatsPanel.tsx
@@ -32,14 +32,18 @@ const AdminStatsPanel = () => {
         })();
     }, []);
     return (
-        <div className="flex justify-evenly w-full mt-4">
-            <AdminStat name="Projects" value={stats.projects} />
-            <AdminStat name="Seen" value={stats.seen} />
-            <AdminStat name="Votes" value={stats.votes} />
-            <AdminStat name="Judging Time" value="01:46:23" />
-            <AdminStat name="Average Mu" value={stats.avg_mu} />
-            <AdminStat name="Average Sigma^2" value={stats.avg_sigma} />
-            <AdminStat name="Judges" value={stats.judges} />
+        <div className="flex flex-row mt-8 w-full">
+            <div className="flex justify-evenly basis-2/5">
+                <AdminStat name="Projects" value={stats.projects} />
+                <AdminStat name="Seen" value={stats.seen} />
+                <AdminStat name="Votes" value={stats.votes} />
+            </div>
+            <AdminStat name="Judging Time" value="01:46:23" className="basis-1/5" />
+            <div className="flex justify-evenly basis-2/5">
+                <AdminStat name="Average Mu" value={stats.avg_mu} />
+                <AdminStat name="Average Sigma^2" value={stats.avg_sigma} />
+                <AdminStat name="Judges" value={stats.judges} />
+            </div>
         </div>
     );
 };
