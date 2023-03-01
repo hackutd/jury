727cec1acf9510def7a611262d2074ff6dd670f1
Michael Zhao <michaelzhao314@gmail.com>
2023-02-28 18:22:02 -0600

    WIGGLE

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -4,7 +4,11 @@ import Container from './components/Container';
 const App = () => {
     return (
         <Container>
-            <h1 className="text-7xl font-bold text-center mb-4">Jury</h1>
+            <h1 className="text-7xl font-bold text-center mb-4 hover:animate-wiggle cursor-pointer">
+                <a href="https://github.com/acmutd/jury" target="_blank" rel="noopener noreferrer">
+                    Jury
+                </a>
+            </h1>
             <h2 className="text-primary text-3xl text-center font-bold mb-24">
                 {process.env.REACT_APP_JURY_NAME}
             </h2>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,13 +10,27 @@ module.exports = {
             lightest: '#C7CFD3',
             background: '#F5F5F5',
             backgroundDark: '#D4DBDF',
-            error: '#EA609C'
+            error: '#EA609C',
         },
         fontFamily: {
             text: ['Rubik', 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'],
             mono: ['Roboto Mono', 'Courier New', 'Courier', 'monospace'],
         },
-        extend: {},
+        extend: {
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': {
+                        transform: 'skew(2deg, 5deg)',
+                    },
+                    '50%': {
+                        transform: 'skew(-5deg, -2deg)',
+                    },
+                },
+            },
+            animation: {
+                wiggle: 'wiggle 0.3s cubic-bezier(.23,.7,.3,.85) infinite',
+            },
+        },
     },
     plugins: [],
 };
