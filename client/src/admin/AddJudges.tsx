a34fbfd09f3d30a27b54eda737c856296bc54a0c
Michael Zhao <michaelzhao314@gmail.com>
2023-05-17 23:38:20 -0500

    I don't know why need so much nesting

--- client/src/admin/AddJudges.tsx
+++ client/src/admin/AddJudges.tsx
@@ -1,11 +1,20 @@
 import JuryHeader from '../components/JuryHeader';
-import AddJudgesPanel from '../components/admin/add-judges/AddJudgesPanel';
+import AddJudgeStatsPanel from '../components/admin/add-judges/AddJudgeStatsPanel';
+import NewJudgeForm from '../components/admin/add-judges/NewJudgeForm';
+import UploadCSVForm from '../components/admin/add-judges/UploadCSVForm';
 
 const AddJudges = () => {
     return (
         <>
             <JuryHeader withLogout isAdmin />
-            <AddJudgesPanel />
+            <div className="flex flex-col items-start justify-center w-full px-8 py-4 md:px-16 md:py-8">
+                <h1 className="text-4xl font-bold">Add Judges</h1>
+                <AddJudgeStatsPanel />
+                <div className="mt-8 flex flex-col w-full space-y-8">
+                    <NewJudgeForm />
+                    <UploadCSVForm />
+                </div>
+            </div>
         </>
     );
 };
