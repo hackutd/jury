8c818b9e8b2e4a81a1c818920c997bda500d2420
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-09-28 18:08:58 -0500

    Update toggle switch UI

--- client/src/components/admin/AdminToggleSwitch.tsx
+++ client/src/components/admin/AdminToggleSwitch.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
 
 const AdminToggleSwitch = (props: {
     state: boolean;
@@ -7,22 +6,43 @@ const AdminToggleSwitch = (props: {
 }) => {
     return (
         <div className="w-full flex flex-row items-center justify-center my-10">
-            <button
-                className={twMerge("block px-4 py-2 text-3xl rounded-full", props.state ? "bg-primary text-white" : "bg-backgroundDark")}
-                onClick={() => {
-                    props.setState(true);
-                }}
-            >
-                Projects
-            </button>
-            <button
-                className={twMerge("block px-4 py-2 text-3xl rounded-full", !props.state ? "bg-primary text-white" : "bg-backgroundDark")}
-                onClick={() => {
-                    props.setState(false);
-                }}
-            >
-                Judges
-            </button>
+            <div className="bg-primaryLight flex flex-row rounded-full relative">
+                <div
+                    className={`absolute top-0 left-0 w-44 h-16 transition-transform duration-300 ease-in-out ${
+                        !props.state ? 'transform translate-x-44' : ''
+                    }`}
+                >
+                    <button
+                        className="block w-full h-full text-2xl rounded-full bg-primary text-white"
+                        onClick={() => {
+                            props.setState(!props.state);
+                        }}
+                    >
+                        {props.state ? 'Projects' : 'Judges'}
+                    </button>
+                </div>
+
+                <button
+                    className={`block w-44 h-16 text-2xl rounded-full ${
+                        props.state ? '' : 'bg-transparent text-light'
+                    }`}
+                    onClick={() => {
+                        if (!props.state) props.setState(!props.state);
+                    }}
+                >
+                    Projects
+                </button>
+                <button
+                    className={`block w-44 h-16 text-2xl rounded-full ${
+                        !props.state ? '' : 'bg-transparent text-light'
+                    }`}
+                    onClick={() => {
+                        if (props.state) props.setState(!props.state);
+                    }}
+                >
+                    Judges
+                </button>
+            </div>
         </div>
     );
 };
