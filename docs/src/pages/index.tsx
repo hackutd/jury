d0d93549a87f3b5e8e4347784a6ac1445443dd65
Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>
2024-04-06 00:08:56 -0600

    Added link to test site on main page (#93)

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,6 +20,31 @@ function HomepageHeader() {
                         Get Started 🚀
                     </Link>
                 </div>
+                <div className={styles.description}>
+                    <h1>Features</h1>
+                    <ul className={styles.list}>
+                        <li>Automated judging application with one-click deployment</li>
+                        <li>User-friendly judging interface with randomized project assignments</li>
+                        <li>
+                            Full-featured admin dashboard with a variety of configurations and
+                            export options
+                        </li>
+                        <li>
+                            Accurate rank aggregation algorithm with aggregated sub-category scores
+                        </li>
+                        <li>Built with modern technologies to run blazingly fast</li>
+                        <li>Quick and easy import from Devpost</li>
+                        <li>Self-hosting on DigitalOcean and MongoDB Atlas for almost no cost</li>
+                    </ul>
+                    <h1 style={{ marginTop: '2rem' }}>Don't Believe Us?</h1>
+                    <Link
+                        className="button button--primary button--lg"
+                        to="https://jury-dev.mikz.dev"
+                    >
+                        Try it Out Online, right now!
+                    </Link>
+                    <p>^ Admin password is "admin"</p>
+                </div>
             </div>
         </header>
     );
@@ -28,9 +53,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <Layout
-            description="Description will go into a meta tag in <head />"
-        >
+        <Layout description="Description will go into a meta tag in <head />">
             <HomepageHeader />
             <main></main>
         </Layout>
