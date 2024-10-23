f1332d1c29b8d4c3711a918d8e5780cdc8cb0ba2
Michael Zhao <michaelzhao314@gmail.com>
2024-10-23 01:26:09 -0500

    Extract clock state to zustand

--- server/models/clock.go
+++ server/models/clock.go
@@ -29,7 +29,7 @@ func (c *ClockState) Pause() {
 		return
 	}
 	c.Running = false
-	c.PauseTime = GetCurrTime() - c.StartTime
+	c.PauseTime = c.PauseTime + GetCurrTime() - c.StartTime
 }
 
 func (c *ClockState) Resume() {
