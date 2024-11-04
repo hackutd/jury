b2d2c8d1492749672bb126672b3750ffd41a8c20
hong-nguyen13 <nguyenb2105679@student.ctu.edu.vn>
2024-11-04 18:09:58 +0700

    Update login.tsx

--- client/src/pages/judge/login.tsx
+++ client/src/pages/judge/login.tsx
@@ -128,7 +128,7 @@ const JudgeLogin = () => {
                     error={error}
                     setError={setError}
                     errorMessage="Invalid judging code"
-                    className="text-8xl text-center w-4/5"
+                    className="text-8xl text-center w-full"
                 />
                 <p className="text-2xl text-light text-center mx-4 my-12">
                     If you did not get a code, check your email or contact an organizer.
