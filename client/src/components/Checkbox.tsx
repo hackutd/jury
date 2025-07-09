5cbe39269c8463b9fc7b22f9d1c387fd266ca437
Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>
2025-07-09 15:14:09 -0500

    Disable some settings while judging is running (#294)

--- client/src/components/Checkbox.tsx
+++ client/src/components/Checkbox.tsx
@@ -10,6 +10,9 @@ interface CheckboxProps {
     /* Function to set state variable */
     onChange: (checked: boolean) => void;
 
+    /* Disable the checkbox */
+    disabled?: boolean;
+
     /* Optional class name */
     className?: string;
 }
@@ -19,17 +22,22 @@ const Checkbox = (props: CheckboxProps) => {
         <div
             className={twMerge(
                 'p-2 cursor-pointer text-light hover:text-black duration-200',
+                props.disabled && 'cursor-not-allowed hover:text-light',
                 props.className
             )}
-            onClick={() => props.onChange(!props.checked)}
+            onClick={() => props.disabled || props.onChange(!props.checked)}
         >
             <input
                 type="checkbox"
                 checked={props.checked}
+                disabled={props.disabled}
                 onChange={(e) => {
-                    props.onChange(e.target.checked);
+                    props.disabled || props.onChange(e.target.checked);
                 }}
-                className="mr-4 rounded-sm bg-white border-primary border-2 text-primary focus:ring-0"
+                className={twMerge(
+                    'mr-4 rounded-sm bg-white border-primary border-2 text-primary focus:ring-0',
+                    props.disabled && 'text-lighter border-lighter'
+                )}
             />
             <p className="inline">{props.children}</p>
         </div>
