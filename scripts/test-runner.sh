dc7801d17c8851aab3373f51dca2f47a7ec31232
Michael Zhao <michaelzhao314@gmail.com>
2026-05-18 23:42:00 -0500

    ACTUAL NEWLINE UGH

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -28,7 +28,7 @@ printf "$logs"
 # If no failed lines, exit with success
 failed=$(echo "$logs" | grep "failed")
 if [[ -z "$failed" ]]; then
-    echo "\n\nSuccess :)"
+    printf "\n\nSuccess :)\n"
     exit 0
 fi
 
